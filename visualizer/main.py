3d8bc8c2e7cd5d33ec1d2ad543dd1e835b765ab5
Max Chateau <mx.chateau@axithem.fr>
2026-01-12 23:57:40 +0100

    wip: first commit for visualizer using streamlit

---
+++ visualizer/main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title("Projet Data Science - Les Français.es et les médias")
+st.markdown(
+    """ 
+     Travail réalisé par :
+        - Max Chateau
+        - Rasim Erben
+        - Myndie Ferrandez
+        - Quentin Jacquot
+    """
+)
+
+if st.button("Send balloons!"):
+    st.balloons()
